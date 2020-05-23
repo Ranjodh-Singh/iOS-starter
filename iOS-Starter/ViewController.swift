8b64a103e9d4d24365f82dab856ac0739765222e
ranjodh singh <ranjodh.singh@salesforce.com>
2020-05-23 19:40:59 +0530

    Initial Commit

---
+++ iOS-Starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Starter
+//
+//  Created by Ranjodh Singh on 5/23/20.
+//  Copyright © 2020 Ranjodh Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-StarterTests/iOS_StarterTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_StarterTests.swift
+//  iOS-StarterTests
+//
+//  Created by Ranjodh Singh on 5/23/20.
+//  Copyright © 2020 Ranjodh Singh. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Starter
+
+class iOS_StarterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
